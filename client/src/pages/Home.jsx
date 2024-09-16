1d93130ca32af912fbc544c1bb64a8628ee197ea
bcbrauneis <bcbrauneis@gmail.com>
2024-09-16 18:53:27 -0500

    added css to new main branch. positioning of some elements are not in correct positoin yet

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,47 +1,61 @@
 // This is a static page mocking an "About Us" section for our fake user data
 export default function Home() {
     return (
-      <section className="page-section" id="work">
-        <h2>Work</h2>
+      <section className="page-section">
+        <h1 className="name">Cause Connect</h1>
         <div className="flex-container">
   
-          <a href="https://projectend-1.onrender.com" className="flex-item projectend">
+          <div className="abt-us">
             <div>
-              <h3>ProjecTend</h3>
-              <span>HTML, CSS, Javascript, our own API, SortableJS, Express.JS, Bcrypt, express handlebars</span>
+              <h3>About Us</h3>
+              <span>  Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+          tenetur maiores, dolor iusto dolorum ullam, natus deleniti blanditiis
+          impedit suscipit sed magnam alias in, repellat expedita hic explicabo
+          architecto soluta. About us Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Velit voluptate exercitationem quaerat pariatur
+          mollitia, excepturi, voluptatem eveniet a dolor nobis ex veniam totam
+          nostrum temporibus ad omnis nam rerum eligendi.</span>
+            </div>
+          </div>
+
+          <div className="abt-us">
+            <div>
+              <h3>Our Mission</h3>
+              <span>  Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+          tenetur maiores, dolor iusto dolorum ullam, natus deleniti blanditiis
+          impedit suscipit sed magnam alias in, repellat expedita hic explicabo
+          architecto soluta. About us Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Velit voluptate exercitationem quaerat pariatur
+          mollitia, excepturi, voluptatem eveniet a dolor nobis ex veniam totam
+          nostrum temporibus ad omnis nam rerum eligendi.</span>
+            </div>
+          </div>
+  
+        
+          <a href="https://www.globalgiving.org" className="flex-item work1 donate">
+            <div>
+              <h3>Donate Now</h3>
+            </div>
+          </a>
+        
+  
+          <a href="https://i1.wp.com/scng-dash.digitalfirstmedia.com/wp-content/uploads/2018/12/charity-stats.jpg" className="flex-item work2 impact">
+            <div>
+              <h3>Our Impact</h3>
             </div>
           </a>
   
-          <a href="https://brandeecheung.github.io/myblog/index.html" className="flex-item work1">
+          <a href="https://www.google.com/search?client=opera-gx&hs=0DJ&sca_esv=de5534b79ca5d3d3&sca_upv=1&q=upcoming+charity+events&spell=1&sa=X&ved=2ahUKEwj78uiV0r6IAxVeMdAFHS2hHYQQBSgAegQIChAB&biw=1879&bih=936&dpr=1" className="flex-item work3 events">
             <div>
-              <h3>Blog</h3>
-              <span>HTML, CSS, JavaScript</span>
-            </div>
-          </a>
-  
-          <a href="https://brandeecheung.github.io/weatherdashboard/index.html" className="flex-item work2">
-            <div>
-              <h3>Weather Dashboard</h3>
-              <span>HTML, CSS, JavaScript, jQuery</span>
-            </div>
-          </a>
-  
-          <a href="https://brandeecheung.github.io/taskboard/index.html" className="flex-item work3">
-            <div>
-              <h3>Taskboard</h3>
-              <span>HTML, CSS, JavaScript</span>
-            </div>
-          </a>
-  
-          <a href="https://brandeecheung.github.io/super-team-project/index.html" className="flex-item work4">
-            <div>
-              <h3>Stocks Generator</h3>
-              <span>HTML, CSS, JavaScript</span>
+              <h3>Up Coming Events</h3>
             </div>
           </a>
   
         </div>
+
+        <footer>
+          <h3>Created by Scam Co. LLC. All donations will not actually go anywhere but our bank accounts. Thank you for your donations!</h3>
+        </footer>
       </section>
     );
   }
-  
